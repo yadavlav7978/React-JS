a2190c8799168dcf75d12908b21bde1f588f9444
Lav Yadav <106106817+yadavlav7978@users.noreply.github.com>
2023-07-01 18:31:33 +0530

    Update index.js

--- 8-(JSX Attributes in ReactJS)/index.js
+++ 8-(JSX Attributes in ReactJS)/index.js
@@ -10,10 +10,12 @@ const img1="https://picsum.photos/300/300";
 const img2="https://picsum.photos/400/400";
 const img3="https://picsum.photos/500/500";
 
+// Here image tag is self closing tag so ( in html we write ---> <img> ) But in jsx it  <img/>
+
 ReactDOM.render(
   <>
     <h1>Welcome to Photos Gallary</h1>
-    <img src="https://picsum.photos/200/300" alt="Random-Images" />
+    <img src="https://picsum.photos/200/300" alt="Random-Images" />  
     <img src={img1} alt="Random-Images" />
     <img src={img2} alt="Random-Images" />
 
