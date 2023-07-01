dd88ca10beca025e17a881d6d99fa2bbd8b1248a
Lav Yadav <106106817+yadavlav7978@users.noreply.github.com>
2023-07-01 11:16:08 +0530

    Hello World in js

---
+++ index.js
@@ -0,0 +1,21 @@
+// ---------------------- ------------- Tutorial: 1 --------------------------------------------------------
+
+ import React from 'react'
+//var React = require('react');
+import ReactDOM from 'react-dom';
+//var ReactDOM = require('react-dom');
+
+
+// ReactDOM.render("Kya dikhana hai (What to show)", "Kha dikhana hai (Where to show)" ,'Callback function');
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+
+
+//React is a JavaScript library for building user interfaces, 
+// and Babel helps developers write React code using modern JavaScript features that may not be supported by all browsers
+// So Babel convert this modern javascript code into that format that understand by browser.
+
+// Note** : JSX is a syntactic extension of JavaScript that allows you to write HTML-like code within your JavaScript code. When you use JSX in your React components, it gets transformed into regular JavaScript code before being executed.
+
+
+
+
