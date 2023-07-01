f81f1d17d7a51471168292ec56b873726b22ec0a
Lav Yadav <106106817+yadavlav7978@users.noreply.github.com>
2023-07-01 18:28:50 +0530

    Add files via upload

---
+++ 8-(JSX Attributes in ReactJS)/index.js
@@ -0,0 +1,30 @@
+//--------------------------------------------JSX Attributes in ReactJS----------------------------------
+
+// Note**: It's important to note that JSX is not HTML. JSX is a syntax extension for JavaScript that allows you to write HTML-like code within JavaScript.
+// The JSX code is then transformed into regular JavaScript by Babel before being executed by the browser.
+
+
+import react from 'react';
+import ReactDOM from 'react-dom';
+const img1="https://picsum.photos/300/300";
+const img2="https://picsum.photos/400/400";
+const img3="https://picsum.photos/500/500";
+
+ReactDOM.render(
+  <>
+    <h1>Welcome to Photos Gallary</h1>
+    <img src="https://picsum.photos/200/300" alt="Random-Images" />
+    <img src={img1} alt="Random-Images" />
+    <img src={img2} alt="Random-Images" />
+
+    <a href='https://www.google.com/' target="_lavyadav" >
+    <img src={img3} alt="Random-Images" />
+    </a>
+
+  </>,
+    document.getElementById('root')
+);
+
+
+
+
